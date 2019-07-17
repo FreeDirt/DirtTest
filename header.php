ebecb2f901df19e1ff6713ddb7f226866b7d5153
Ryan Mendoza <r.mendoza@acc.dr-klippe.de>
2019-07-17 15:27:37 +0800

    change header

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 			<?php
 			$custom_logo_id = get_theme_mod( 'custom_logo' );
 			$custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' ); ?>
-			<a href="<?php echo esc_url(home_url( '/'));  ?>"><?php echo '<img src="' . esc_url( $custom_logo_url ) . '" alt=""> '?></a>';               
+			<a href="<?php echo esc_url(home_url( '/'));  ?>"><?php echo '<img src="' . esc_url( $custom_logo_url ) . '" alt="flood control asia logo"> '?></a>       
 			<?php ?>
 
 			
